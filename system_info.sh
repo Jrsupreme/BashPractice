d691c04e0beba8bcff3d51038a5b4f1d9b1a17dd
Jrsupreme <jpenakuralabs@outlook.com>
2024-08-27 23:32:03 -0400

    minor

--- system_info.sh
+++ system_info.sh
@@ -35,9 +35,9 @@ echo "The system has $cpu_available CPU(S)"
 
 #function to get memory information
 get_mem_info() {
-  memory_total=$(free -m | awk '/Mem:/ {print $2}')
-  memory_used=$(free -m | awk '/Mem:/ {print $3}')
-  memory_free=$(($memory_total - $memory_used))
+  memory_total=$(free -m | awk '/Mem:/ {print $2}') #awk is great.
+  memory_used=$(free -m | awk '/Mem:/ {print $3}') # fetches the memory value numbers  from the free command
+  memory_free=$(($memory_total - $memory_used)) #calculating free space
 echo "memory total : $memory_total Mbs"
 echo "memory used : $memory_used Mbs"
 echo "memory free : $memory_free Mbs"
@@ -46,20 +46,20 @@ echo "memory free : $memory_free Mbs"
 #function to get the top 5 memory processes
 get_top5_mem_process() {
  #echo "pmem   pid	  cmd"
-ps aux --sort=-%mem | awk 'NR<=6{print $4, $2 , $11}'
+ps aux --sort=-%mem | awk 'NR<=6{print $4, $2 , $11}' #sort the ps aux based on mem% usage and prints out the first(which should be alredy sorted) 5  line
 }
 
 #function to get top 5 cpu processes
 get_top5_cpu_process(){
-ps aux --sort=-%cpu | awk 'NR<=6{print $3, $2, $11}'
+ps aux --sort=-%cpu | awk 'NR<=6{print $3, $2, $11}' #same as before but for cpu%
 }
 #function to check for internet connection
 check_for_network_connection() {
 read -p "insert url or ip address to connect to " address 
-  ping_output=$(ping -c 4 $address)
+  ping_output=$(ping -c 4 $address) #test for internet connection, lantency and paket loss.
   if echo "$ping_output" | grep -q "0% packet loss" #grep -q checks if for 0% packet loss and return a 0 code which will be useful for a if loop in the next line
    then
-   connection_time=$(echo "$ping_output" | grep 'time=' | awk -F 'time=' '{print $2}' | awk '{print $1;exit}')
+   connection_time=$(echo "$ping_output" | grep 'time=' | awk -F 'time=' '{print $2}' | awk '{print $1;exit}') #grabs the main connection time and stores it in a variable.
    echo "It took $connection_time ms to connect to $address and there was 0% packet loss."
   else
    echo "There was a problem connecting to $address"
@@ -67,7 +67,7 @@ read -p "insert url or ip address to connect to " address
 }
 
 #function for main menu
-systeminfo_main_menu() {
+systeminfo_main_menu() { #made the main menu a function cause I feel like I can use it for a disferent script in the future. Also i'd like to further my understanding of functions
 while true; do #This whike loop will keep the script running until existed
     echo "Please choose an option:"
     echo "1. IP Addresses"
@@ -81,9 +81,9 @@ while true; do #This whike loop will keep the script running until existed
 
     read -p "Enter your choice: " choice
 
-    case $choice in
+    case $choice in #case in this case(pun intented) allows us to execute the function depending if choice = a number from 1 - 8 otherwise it will return a error.
         1)
-            get_ip_addresses
+            get_ip_addresses # if user's choice = 1  or any if the other numbered option the corresponding function will trigger.
             ;;
         2)
             get_current_user
@@ -108,11 +108,12 @@ while true; do #This whike loop will keep the script running until existed
             break
             ;;
         *)
-            echo "Invalid option. Please try again."
+            echo "Invalid option."
             ;;
     esac
 
 done
 echo "system_info.sh exited"
 }
+#run the main menu function
 systeminfo_main_menu
