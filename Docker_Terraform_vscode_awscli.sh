a50c72462db707589809bba779f507b0e82d49b5
Jeremy Pena <09jeremy09@gmail.com>
2024-11-15 20:08:54 -0500

    Update Docker_Terraform_vscode_awscli.sh

--- Docker_Terraform_vscode_awscli.sh
+++ Docker_Terraform_vscode_awscli.sh
@@ -31,13 +31,26 @@ sudo ./aws/install
 rm -rf awscliv2.zip aws
 
 # Install Visual Studio Code
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
-sudo apt update -y
-sudo apt install -y code
-rm -f packages.microsoft.gpg
+Install code-server
+curl -L https://code-server.dev/install.sh | sh
 
+# Enable and start code-server service using systemctl
+sudo systemctl enable --now code-server@$USER
+
+# Brief pause to ensure the service is properly enabled
+sleep 1
+
+# Restart the code-server service
+sudo systemctl restart code-server@$USER
+
+# Configure code-server with custom settings
+CONFIG_PATH="$HOME/.config/code-server/config.yaml"
+
+# Write configuration to the config.yaml file
+echo "bind-addr: 0.0.0.0:8081
+auth: password
+password: abcd1234
+cert: false" > "$CONFIG_PATH"
 # Clean up
 sudo apt autoremove -y
 sudo apt clean
