a56867ed6aabc94cad2f613e4c0cd01cff8f9154
Jeremy Pena <09jeremy09@gmail.com>
2024-11-15 19:04:31 -0500

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt update
+sudo apt update -y
 sudo apt install fontconfig openjdk-17-jre -y
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
